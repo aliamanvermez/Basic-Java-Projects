c0185a19804bf34b433e5d86cb5b929bd8189bcd
Muhammet Ali Amanvermez <64315684+aliamanvermez@users.noreply.github.com>
2021-10-13 20:13:58 +0300

    Update Convert.java

--- Convert.java
+++ Convert.java
@@ -1,3 +1,19 @@
+
+/*
+Given that strings are immutable, we cannot directly edit an instance to encrypt
+it. Instead, our goal will be to generate a new string. A convenient technique for
+performing string transformations is to create an equivalent array of characters, edit
+the array, and then reassemble a (new) string based on the array.
+Java has support for conversions from strings to character arrays and vice versa.
+Given a string S, we can create a new character array matching S by using the
+method, S.toCharArray( ). For example, if s="bird", the method returns the character
+array A={'b', 'i', 'r', 'd'}. Conversely, there is a form of the String
+constructor that accepts a character array as a parameter. For example, with character
+array A={'b', 'i', 'r', 'd'}, the syntax new String(A) produces "bird".
+
+Source : Data Structures and Algorithms in Java, 6th Edition
+*/
+
 import java.util.*;
 
 public class Convert {
