57efd2defe8a4dabe486855c24768c166af2a7c9
Muhammet Ali Amanvermez <64315684+aliamanvermez@users.noreply.github.com>
2021-10-13 21:19:20 +0300

    Update Cipher.java

--- CaesarCipher/Cipher.java
+++ CaesarCipher/Cipher.java
@@ -47,7 +47,7 @@ public class Cipher {
         System.out.println("Encryption code = " + new String(cipher.encoder));
         System.out.println("Decryption code = " + new String(cipher.decoder));
 
-        String message = "ALI EVE GEL";
+        String message = "APPLE";
 
         String coded = cipher.encrypt(message);
         System.out.println("Secret:" + coded);
