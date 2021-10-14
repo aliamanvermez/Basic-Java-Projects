6110d58788c5f17d68a6901de904fdf4e48fe18c
Muhammet Ali Amanvermez <64315684+aliamanvermez@users.noreply.github.com>
2021-10-14 12:47:16 +0300

    Update TicTacToe.java

--- TicTacToe(Basic)/TicTacToe.java
+++ TicTacToe(Basic)/TicTacToe.java
@@ -76,7 +76,7 @@ public class Projects {
     public static void main(String[] args) {
         Projects game = new Projects();
 
-        //X Moves
+        //X Moves                  //Y Moves
         game.putMark(1,1);         game.putMark(0,2);
         game.putMark(2,2);         game.putMark(0,0);
         game.putMark(0,1);         game.putMark(2,1);
